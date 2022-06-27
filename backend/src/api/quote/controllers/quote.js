aa8a6dfea277ef80843cae46bcf2e861f2ce9a46
i1d9 <42600121+i1d9@users.noreply.github.com>
2022-06-27 23:13:03 +0300

    Extended Qoutes

--- backend/src/api/quote/controllers/quote.js
+++ backend/src/api/quote/controllers/quote.js
@@ -6,4 +6,24 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::quote.quote');
+module.exports = createCoreController('api::quote.quote', ({ strapi }) => ({
+    create(ctx) {
+        return strapi.service('api::quote.quote').createQuote({
+            data: {
+                ...ctx.request.body.data,
+                owner: ctx.state.user.id
+            }
+        });
+    },
+    async find(ctx) {
+        //Get Quotes with loaded relations
+        let entities = await strapi.service('api::quote.quote').loadQuotes();
+        entities = entities.map((entity) => {
+            //Check if the authenicated User has liked the quote
+            var result = entity.likes.find(like => like.owner.id === ctx.state.auth.credentials.id);
+            return { ...entity, liked: result ? true : false }
+        });
+        return entities;
+
+    }
+}));
--- backend/src/api/quote/services/quote.js
+++ backend/src/api/quote/services/quote.js
@@ -6,4 +6,46 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::quote.quote');
+module.exports = createCoreService('api::quote.quote', ({ strapi }) => ({
+
+    loadQuotes() {
+        return strapi.entityService.findMany('api::quote.quote', {
+            populate: {
+                owner: {
+                    fields: ['username']
+                },
+                likes: {
+                    populate: {
+                        owner: {
+                            fields: ['id'],
+                        }
+                    }
+                }
+            }
+        });
+    },
+    createQuote(params) {
+
+
+        return super.create(params);
+
+    },
+
+    findQuote(id) {
+
+
+        return strapi.entityService.findOne('api::quote.quote', id, {
+
+            populate: {
+                owner: {
+                    fields: ['fcm']
+                }
+            }
+        });
+
+    }
+
+
+
+
+}));
