d6ac9a9e711d1f42651c5a54186b4cb3bdbd327b
Ian Nalyanya <42600121+i1d9@users.noreply.github.com>
2022-07-12 16:26:25 +0300

    Home Page

---
+++ frontend/lib/screens/splash.dart
@@ -0,0 +1,12 @@
+part of 'package:frontend/helpers/import.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Splash View"),
+    );
+  }
+}
